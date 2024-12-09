eccc539ead257cc7330f07acf935db33fc52b0f9
Devanshi Bilthare <100258554+Devanshi-Bilthare@users.noreply.github.com>
2024-12-09 10:18:33 +0530

    added base url

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,2 +1,2 @@
-export const base_url = 'http://localhost:4000/api/'
-// export const base_url = 'https://shadi-mahotsav-backend.vercel.app/api/'
+// export const base_url = 'http://localhost:4000/api/'
+export const base_url = 'https://shadi-mahotsav-backend.vercel.app/api/'
